3698b40654cc4d4d38b98e8e5c5b621df68cb0a8
Nino Burtchuladze <burtchuladze.dev@gmail.com>
2024-08-11 11:56:36 +0400

    initial commit

---
+++ projects/content-projection/alert-box/alert-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './alert-box.component.html',
+  styleUrl: './alert-box.component.css'
+})
+export class AlertBoxComponent {
+
+}
